85ff2ffaa283060f6cdf10a09c22cabe7eccc5ca
Li Wei <724399396@qq.com>
2015-09-19 23:27:52 +0800

    9.19 win10

--- ch09/BetterPredicate.hs
+++ ch09/BetterPredicate.hs
@@ -42,7 +42,7 @@ simpleFileSize path = do
     return size
 
 saferFileSize :: FilePath -> IO (Maybe Integer)
-saferFileSize path = do    
+saferFileSize path = do
         h <- openFile path ReadMode
         size <- hFileSize h
         hClose h
@@ -52,3 +52,60 @@ myTest path _ (Just size) _ =
     takeExtension path == ".cpp" && size > 131072
 myTest _ _ _ _ = False
 
+type InfoP a = FilePath
+            -> Permissions
+            -> Maybe Integer
+            -> UTCTime
+            -> a
+
+pathP :: InfoP FilePath
+pathP path _ _ _ = path
+
+sizeP :: InfoP Integer
+sizeP _ _ (Just size) _ = size
+sizeP _ _ Nothing _ = -1
+
+equalP :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP f k = \w x y z -> f w x y z == k
+
+equalP' :: (Eq a) => InfoP a -> a -> InfoP Bool
+equalP' f k w x y z = f w x y z == k
+
+liftP :: (a -> b -> c) -> InfoP a -> b -> InfoP c
+liftP q f k w x y z = f w x y z `q` k
+
+greaterP, lesserP :: (Ord a) => InfoP a -> a -> InfoP Bool
+greaterP = liftP (>)
+lesserP = liftP (<)
+
+simpleAndP :: InfoP Bool -> InfoP Bool -> InfoP Bool
+simpleAndP f g w x y z = f w x y z && g w x y z
+
+liftP2 :: (a -> b -> c) -> InfoP a -> InfoP b -> InfoP c
+liftP2 q f g w x y z = f w x y z `q` g w x y z
+
+andP =  liftP2 (&&)
+orP = liftP2 (||)
+
+constP :: a -> InfoP a
+constP k _ _ _ _ = k
+
+liftP' q f k w x y z = f w x y z `q` constP k w x y z
+
+liftPath :: (FilePath -> a)  -> InfoP a
+liftPath f w _ _ _ = f w
+
+myTest2 = (liftPath takeExtension `equalP` ".cpp") `andP`
+          (sizeP `greaterP` 131072)
+
+(==?) = equalP
+(&&?) = andP
+(>?) = greaterP
+
+myTest3 = (liftPath takeExtension ==? ".cpp") &&? (sizeP >? 131072)
+
+infix 4 ==?
+infixr 3 &&?
+infix 4 >?
+
+myTest5 = liftPath takeExtenion ==? ".cpp" &&? sizeP >? 131072
---
+++ ch09/ControlledVisit.hs
@@ -0,0 +1,60 @@
+-- file: ch09/ControlledVisit.hs
+import System.Directory (Permissions(..), getModificationTime, getPermissions,getDirectoryContents)
+import Data.Time.Clock (UTCTime)
+import System.FilePath (takeExtension,(</>))
+import Control.Exception (bracket,handle, SomeException)
+import System.IO (IOMode(..), hClose, hFileSize, openFile)
+import Control.Monad (liftM,forM)
+
+data Info = Info {
+    infoPath :: FilePath,
+    infoPerms :: Maybe Permissions,
+    infoSize :: Maybe Integer,
+    infoModTime :: Maybe UTCTime
+} deriving (Eq, Ord, Show)
+
+getInfo :: FilePath -> IO Info
+
+traverse :: ([Info] -> [Info]) -> FilePath -> IO [Info]
+traverse order path = do
+  names <- getUsefulContents path
+  contents <- mapM getInfo (path : map (path </>) names)
+  liftM concat $ forM (order contents) $ \info -> do
+      if isDirectory info && infoPath info /= path
+        then Main.traverse order (infoPath info)
+        else return [info]
+
+getUsefulContents :: FilePath -> IO [String]
+getUsefulContents path = do
+  names <- getDirectoryContents path
+  return (filter (`notElem` [".", ".."]) names)
+
+isDirectory :: Info -> Bool
+isDirectory = maybe False searchable . infoPerms
+
+maybeIO :: IO a -> IO (Maybe a)
+maybeIO act = handleAny (\_ -> return Nothing) (Just `liftM` act)
+
+handleAny :: (SomeException -> IO a) -> IO a -> IO a
+handleAny = handle
+
+getInfo path = do
+  perms <- maybeIO (getPermissions path)
+  size <- maybeIO (bracket (openFile path ReadMode) hClose hFileSize)
+  modified <- maybeIO (getModificationTime path)
+  return (Info path perms size modified)
+
+  traverseVerbose order path = do
+    names <- getDirectoryContents path
+    let usefulNames = filter (`notElem` [".",".."]) names
+    contents <- mapM getEntryName ("" : usefulNames)
+    recursiveContents <- mapM recurse (order contents)
+    return (concat recursiveContents)
+  where getEntryName name = getInfo (path </> name)
+        isDirectory info = case infoPerms info of
+                                Nothing -> false
+                                Just perms -> searchable perms
+        recurse info = do
+          if isDirectory info && infoPath info /= path
+            then traverseVerbose order (infoPath info)
+            else return [info]
