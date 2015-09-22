aebffb7b90bb81a5f9acbe7f34cda0e3535180c3
Li Wei <724399396@qq.com>
2015-09-22 22:23:24 +0800

    9.22 win10

--- ch13/num.hs
+++ ch13/num.hs
@@ -15,7 +15,7 @@ arithmetic operation (such as cos)
 
 Notice the types of BinaryArith and UnaryArith: it's a recursive
 type.  So, we could represent a (+) over two SymbolicManips. -}
-data SymbolicManip a = 
+data SymbolicManip a =
           Number a           -- Simple number, such as 5
         | Symbol String      -- A symbol, such as x
         | BinaryArith Op (SymbolicManip a) (SymbolicManip a)
@@ -76,7 +76,7 @@ prettyShow (BinaryArith op a b) =
         pb = simpleParen b
         pop = op2str op
         in pa ++ pop ++ pb
-prettyShow (UnaryArith opstr a) = 
+prettyShow (UnaryArith opstr a) =
     opstr ++ "(" ++ show a ++ ")"
 
 op2str :: Op -> String
@@ -88,7 +88,7 @@ op2str Pow = "**"
 
 {- Add parenthesis where needed.  This function is fairly conservative
 and will add parenthesis when not needed in some cases.
-    
+
 Haskell will have already figured out precedence for us while building
 up the SymbolicManip. -}
 simpleParen :: (Show a, Num a) => SymbolicManip a -> String
@@ -118,11 +118,11 @@ rpnShow i =
 {-- snippet simplify --}
 {- Perform some basic algebraic simplifications on a SymbolicManip. -}
 simplify :: (Num a) => SymbolicManip a -> SymbolicManip a
-simplify (BinaryArith op ia ib) = 
+simplify (BinaryArith op ia ib) =
     let sa = simplify ia
         sb = simplify ib
         in
-        case (op, sa, sb) of 
+        case (op, sa, sb) of
                 (Mul, Number 1, b) -> b
                 (Mul, a, Number 1) -> a
                 (Mul, Number 0, b) -> Number 0
@@ -134,3 +134,70 @@ simplify (BinaryArith op ia ib) =
                 _ -> BinaryArith op sa sb
 simplify (UnaryArith op a) = UnaryArith op (simplify a)
 simplify x = x
+
+{- New date type: Units. A Units type contains a number
+and a SymbolicManip, which represents the units of measure
+A simple label would be something like (Symbol "m") -}
+data Num a => Units a = Units a (SymbolicManip a)
+            deriving (Eq)
+
+instance (Num a) => Num (Units a) where
+       (Units xa ua) + (Units xb ub)
+           | ua == ub = Units (xa + xb) ua
+           | otherwise = error "Mis-mathced units in add or substract"
+       (Units xa ua) - (Units xb ub) = (Units xa ua) + (Units (xb * (-1)) ub)
+       (Units xa ua) * (Units xb ub) = Units (negate xa) ua
+       negate (Inits xa ua) = Units (negate xa) ua
+       abs (Units xa ua) = Units (abs xa) ua
+       signum (Units xa _) = Units (signum xa) (Number 1)
+       fromInteger i = Units (fromInteger i) (Number 1)
+
+instance (Fractional a) => Fractional (Units a) where
+    (Units xa ua) / (Units xb ub) = Units (xa / xb) (ua / ub)
+    recip a = 1 / a
+    fromRational r = Units (fromRational r) (Number 1)
+
+instance (Floating a) => Floating (Units a) where
+  pi = (Units pi (Number 1))
+  exp _ = error "exp not yet implemented in Units"
+  log _ = error "log not yet implemented in Units"
+  (Units xa ua) ** (Units xb ub)
+      | ub == Number 1 = Units (xa ** xb) (ua ** Number xb)
+      | otherwise = error "units for RHS of ** not supported"
+  sqrt (Units xa ua) = Units (sqrt xa) (sqrt ua)
+  sin (Units xa ua)
+      | ua == Symbol "rad" = Units (sin xa) (Number 1)
+      | ua == Symbol "deg" = Units (sin (deg2rad xa)) (Number 1)
+      | otherwise = error "Units for sin must be deg or rad"
+  cos (Units xa ua)
+      | ua == Symbol "rad" = Units (cos xa) (Number 1)
+      | ua == Symbol "deg" = Units (cos (deg2rad xa)) (Number 1)
+      | otherwise = error "Units for cos must be deg or rad"
+  tan (Units xa ua)
+      | ua == Symbol "rad" = Units (tan xa) (Number 1)
+      | ua == Symbol "deg" = Units (tan (deg2rad xa)) (Number 1)
+      | otherwise = error "Units for tan must be deg or rad"
+  asin (Units xa ua)
+      | ua == Number 1 = Units (rad2deg $ asin xa) (Symbol "deg")
+      | otherwise = error "Units for asin must be empty"
+  acos (Units xa ua)
+      | ua == Number 1 = Units (rad2deg $ acos xa) (Symbol "deg")
+      | otherwise = error "Units for acos must be empty"
+  atan (Units xa ua)
+      | ua == Number 1 = Units (rad2deg $ atan xa) (Symbol "deg")
+      | otherwise = error "Units for atan must be empty"
+  sinh = error "sinh not yet implemented in Units"
+  cosh = error "cosh not yet implemented in Units"
+  tanh = error "tanh not yet implemented in Units"
+  asinh = error "asinh not yet implemented in Units"
+  acosh = error "acosh not yet\  implemented in Units"
+  atanh = error "atanh not yet implemented in Units"
+
+units :: (Num z) => z -> String -> Units z
+units a b = Units a\   (Symbol b)
+
+dropUnits :: (Num z) => Units z -> z
+dropUnits (Units x _) = x
+
+deg2rad x = 2 * pi * x / 360
+rad2deg x = 360 * x / (2 * pi)
