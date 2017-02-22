dd37b246a52782bf442c22c096af7b38e69da54c
liww.li <liww.li@qunar.com>
2017-02-22 18:33:22 +0800

    197

--- ch07/callingpure.hs
+++ ch07/callingpure.hs
@@ -2,7 +2,7 @@
 name2reply :: String -> String
 name2reply name =
     "Pleased to meet you, " ++ name ++ ".\n" ++
-    "Your name contains " ++ charcount ++ "characters."
+    "Your name contains " ++ charcount ++ " characters."
     where charcount = show (length name)
 
 main :: IO ()
